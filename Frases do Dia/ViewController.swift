075b192966e8ebaf27118ce480294e7d7e986280
Victor Rodrigues Novais <victor96novais@gmail.com>
2020-04-21 15:48:26 -0300

    POC

--- Frases do Dia/ViewController.swift
+++ Frases do Dia/ViewController.swift
@@ -9,6 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var legendaResultado: UILabel!
+    
+    @IBAction func novaFrase(_ sender: Any) {
+        
+        var frases: [String] = []
+        frases.append("O importante não é vencer todos os dias, mas lutar sempre.")
+        frases.append("Maior que a tristeza de não haver vencido é a vergonha de não ter lutado!")
+        frases.append("É melhor conquistar a si mesmo do que vencer mil batalhas.")
+        frases.append("Quem ousou conquistar e saiu pra lutar, chega mais longe!")
+        frases.append("Enquanto houver vontade de lutar haverá esperança de vencer.")
+        frases.append("Difícil é ganhar um amigo em uma hora; fácil é ofendê-lo em um minuto.")
+        frases.append("O medo de perder tira a vontade de ganhar.")
+        frases.append("Aquele que não tem confiança nos outros, não lhes pode ganhar a confiança.")
+        
+        let randomNumber = arc4random_uniform(6)
+        
+        legendaResultado.text = frases[Int(randomNumber)]
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
